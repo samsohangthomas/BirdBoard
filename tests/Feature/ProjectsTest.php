7208f7d96fbb12fcbf074fddab528dd817fd8b8d
Thomas Samsohang <samsohangthomas@Thomass-MacBook-Pro.local>
2022-11-29 18:23:24 +0545

    Unit Test in progress

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+
+    use WithFaker, RefreshDatabase;
+    
+    /** @test **/
+    public function a_user_can_create_project(){
+
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+
+        ];
+
+        $this->post('/projects',$attributes);
+
+
+        $this->assertDatabaseHas('projects',$attributes);
+
+    }
+}
